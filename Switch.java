a8f4b29a6aee7c5228c60330d2f985585c16a593
Priyanshu20048 <priyanshu2500.be23@chitkara.edu.in>
2025-01-26 23:59:00 +0530

    changes

---
+++ Switch.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Switch{
+    public static void main(String [] args){
+    Scanner In = new Scanner(System.in);
+    String fruit = in.next();
+    if (fruit == "mango"){
+        
+    }
+    }
+}
