38be0ac9a09b20b7721f171d688c01467617c001
Priyanshu20048 <mittal.priyanshu2004@gmail.com>
2025-04-14 18:44:58 +0530

    Richest Customer Wealth

---
+++ Lectures/Searching/Linear Search/MaxWealth.java
@@ -0,0 +1,19 @@
+public class MaxWealth {
+    public static void main(String[] args){
+
+    }    
+public int maximumWealth(int[][] accounts){
+    int ans = Integer.MIN_VALUE;
+    for (int person =0;person < accounts.length;person++){
+        // when you start a new column,take anew for that row  
+        int sum =0;
+         for (int account =0; account < accounts[person].length;account++){
+           sum+= accounts[person][account];
+        }
+        // now we have sum of accounts of person 
+        // check with overall ans 
+        if (sum > ans ){
+            ans = sum;
+        }
+    }
+}
