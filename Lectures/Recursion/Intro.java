60ea8bc4998a76bcad3df97c318197760325c823
Priyanshu20048 <mittal.priyanshu2004@gmail.com>
2025-04-28 15:00:41 +0530

    pending

---
+++ Lectures/Recursion/Intro.java
@@ -0,0 +1,58 @@
+// // write a ufnction that prints helo worls 
+// import java.util.*;
+// public class Intro{
+//     public static void main(String[] args){
+//         message();
+//     }
+// public static void message(){
+//         System.out.println("Hello , my name is Priyanshu");
+//       }
+// }
+
+// // now i want to print it 5 times 
+// // what are you approach ?
+// either call the function 5 times or modify the message () function and write their 5 times 
+// // but what if I dont want to change the strcture of function 
+// then you can do that create some more functions with different name but same body 
+// and call each other in different function like this
+
+// write a ufnction that prints helo worls 
+import java.util.*;
+public class Intro{
+    public static void main(String[] args){
+        message();
+    }
+public static void message(){
+        System.out.println("Hello , my name is Priyanshu");
+        message1();
+      }
+    
+      public static void message1(){
+        System.out.println("Hello , my name is Priyanshu");
+        message2();
+      }
+
+      public static void message2(){
+        System.out.println("Hello , my name is Priyanshu");
+        message3();
+      }
+      public static void message3(){
+        System.out.println("Hello , my name is Priyanshu");
+        message4();
+      }
+      public static void message4(){
+        System.out.println("Hello , my name is Priyanshu");
+      }
+}
+// now here the function will execute and it will call other functionn 
+
+// all the functon calls that happen in programming -> they go into stack memory 
+
+// --------------------------------------------------------
+how function call works in language 
+1. while the functio is not finished executing , it will remain in stack 
+2. when a funtion inishes it executing , it is removed from stack
+ and the flow of program is restored to where that function was called . 
+3. when the last function (main ), function is removed from the stack , the program ends
+
+
--- Lectures/Sorting/Selection_Sort/Introduction.java
+++ Lectures/Sorting/Selection_Sort/Introduction.java
@@ -1,21 +1,36 @@
-package Lectures.Sorting.Selection_Sort;
 
 import java.util.Arrays;
 
 public class Introduction {
     public static void main(String[] args){
-        int[] arr = {5,3,2,1};
-        bubble(arr);
+        int[] arr = {1,7,5,0,-5};
+        selection(arr);
         System.out.println(Arrays.toString(arr));
 }
 
 static void selection(int[] arr){
     for (int i = 0 ; i <arr.length;i++){
-        // find the max item in the reamining array and swap with correct index 
+ // find the max item in the remaining array and swap with correct index 
         int last = arr.length-i-1;
         int maxIndex = getMaxIndex(arr,0,last);
+        swap(arr,maxIndex,last);
     }
 }
-private static int getMaxIndex(int[] arr,int s)
+static void swap (int[] arr,int first,int second){
+    int temp = arr[first];
+    arr[first] = arr[second];
+    arr[second] = temp;
+}
+private static int getMaxIndex(int[] arr,int s,int e){
+    int max= s;
+    for (int i = s ; i <=e ; i++ ){
+        if (arr[max] < arr[i]){
+            max= i;
+        }
+    }
+    return max;
+}
+
+
 }
 
